9d0e049fce43cd01b726b07d3310e22030c43d9b
linzi <linzi4235@gmail.com>
2017-01-18 11:13:50 +0800

    add vuerouter

--- src/components/router.js
+++ src/components/router.js
@@ -1,10 +1,10 @@
 /**
  * Created by linzi on 2017/1/18.
  */
-import content from 'components/content/Content'
+import content from 'components/content/Content';
 
 export default {
-  'index':{
-    component:content
+  'index': {
+    component: content
   }
-}
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
 import App from './App';
-import routerConfing from 'components/router'
-import Router from 'vue-router'
+import routerConfing from 'components/router';
+import Router from 'vue-router';
 
-Vue.use(Router)
-var router =new Router()
-router.map(routerConfing)
-router.start(App,'#app')
+Vue.use(Router);
+var router = new Router();
+router.map(routerConfing);
+router.beforeEach(function () {
+  window.scrollTo(0, 0);
+});
+router.redirect({
+  '*': '/index'
+});
+router.start(App, 'app');
