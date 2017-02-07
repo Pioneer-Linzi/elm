ff84aaa894b79fb7150b4fe4303f52d0102ab35a
linzi <linlin_zhao@yeah.net>
2017-02-07 15:12:29 +0800

    add header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,24 +1,28 @@
 <template>
-    <div class=header>
-	    <div class="content-wrapper">
-		    <div class="avatar">
-			    <img :src="seller.avatar" width="64px" height="64px" alt="" />
-		    </div>
-		    <div class="content">
-		   	<div class="title">
-				this is title
+	<div class=header>
+		<div class="content-wrapper">
+			<div class="avatar">
+				<img :src="seller.avatar" width="64px" height="64px" alt="" />
 			</div>
-			<div class="name">
-				this is name
+			<div class="content">
+				<div class="title">
+					<span class="brand">
+					</span>
+					<span class="name">
+						{{seller.name}}
+					</span>
+				</div>
+				<div class="description">
+					{{seller.description}}/{{seller.deliveryTime}}分钟送达
+				</div>
+				<div v-if="seller-supports" class="support">
+					
+				</div>
 			</div>
-			<div class="description">
-				this is description
-			</div>
-		    </div>
-	    </div>
-	    <div class="bulletin-wrapper">
-	    </div>
-    </div>
+		</div>
+		<div class="bulletin-wrapper">
+		</div>
+	</div>
 </template>
 <style>
 
