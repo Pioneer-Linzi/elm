210a8781c7e09800a19790d37060f453bddeec1b
linzi <linlin_zhao@yeah.net>
2017-02-07 13:21:38 +0800

    add resource

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <div id="app">
-      <ele-header></ele-header>
-      <tab></tab>
-     <router-view></router-view>
-  </div>
+	<div id="app">
+		<ele-header :seller="seller"></ele-header>
+		<tab></tab>
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-    import header from './components/header/Header';
-    import tab from './components/tab/Tab';
-    import content from './components/content/Content';
-    export default {
-        components: {
-            'ele-header': header,
-            tab,
-            content
-        }
-    };
+import header from './components/header/Header';
+import tab from './components/tab/Tab';
+import content from './components/content/Content';
+export default {
+	data () {
+		return {
+			seller: {}
+		};
+	},
+	components: {
+		'ele-header': header,
+		tab,
+		content
+	},
+	created () {
+		this.$http.get('/api/seller').then((response) => {
+			response = response.body;
+			if (response.error === 0) {
+				console.log('response.error');
+			} else {
+				this.seller = response.data;
+			}
+		});
+	}
+};
 </script>
 <style>
-    html {
-        height: 100%;
-    }
+	html {
+		height: 100%;
+	}
 
-    #app {
-        width: 100%;
-        height: 100%;
-    }
+	#app {
+		width: 100%;
+		height: 100%;
+	}
 </style>
