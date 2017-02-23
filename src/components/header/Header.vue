8a6519590b4e96d5c3f62735baf248d17fa18b7e
linzi <linlin_zhao@yeah.net>
2017-02-23 15:03:07 +0800

    header 组件的编写

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -26,6 +26,18 @@
 	</div>
 </template>
 <style lang="stylus" rel="stylesheet/stylus">
+	.header
+		color:#fff;
+		background:#000;
+		.content-wrapper
+			padding: 24px 12px 18px 24px
+            font-size:0
+			.avatar
+				display:inline-block
+			.content
+				display: inline-block
+				margin-left:16px
+				font-size:14px
 
 </style>
 <script type="text-ecmascript-6">
