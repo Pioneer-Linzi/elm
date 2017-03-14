b0f8e00545d6a16fd17c7e3336d9df8df13d7797
linzi <linzi4235@gmail.com>
2017-03-14 17:54:26 +0800

    header 组件的编写

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -30,6 +30,9 @@
             <span class="bulletin-text">{{seller.bulletin}}</span>
             <i class="icon-keyboard_arrow_right"></i>
         </div>
+        <div class="background">
+            <img :src="seller.avatar" width="100%" height='100%'; alt="">
+        </div>
     </div>
 </template>
 <style lang="stylus" rel="stylesheet/stylus">
@@ -37,7 +40,7 @@
 
     .header
         color:#fff
-        background:rgba(0,0,0,0.2)
+        background:rgba(7,17,27,0.5);
         .content-wrapper
             position:relative
             padding: 24px 12px 18px 24px
@@ -135,7 +138,14 @@
                 font-size:10px
                 right: 12px
                 top:8px
-
+        .background
+            position: absolute
+            top:0
+            left:0
+            width:100%
+            height:100%
+            z-index: -1
+            filter:blur(10px)
 
 </style>
 <script type="text-ecmascript-6">
