4ea4f647c70b7e08145320223c83d8c4806a8f17
linzi <linzi4235@gmail.com>
2017-05-15 14:14:34 +0800

    css sticky footer

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -37,6 +37,7 @@
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
+          <star :size="48" :score="seller.score"></star>
         </div>
       </div>
       <div class="detail-close">
@@ -168,26 +169,27 @@
       overflow: auto
       background: rgba(7, 17, 27, 0.8)
       .detail-wrapper
-        min-height:100%
-        width:100%
+        min-height: 100%
+        width: 100%
         .detail-main
           margin-top: 64px
-          padding-bottom:64px
+          padding-bottom: 64px
           .name
-            line-height:16px
+            line-height: 16px
             text-align: center
             font-size: 16px
-            font-weight:700
+            font-weight: 700
       .detail-close
-        position:relative
-        width:32px
+        position: relative
+        width: 32px
         height: 32px
-        margin:-64px auto
-        clear:both
-        font-size:32px
+        margin: -64px auto
+        clear: both
+        font-size: 32px
 
 </style>
 <script>
+  import star from 'components/star/star';
   export default {
     data () {
       return {
@@ -206,6 +208,9 @@
     },
     created () {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    components: {
+      star: star
     }
   };
 </script>
