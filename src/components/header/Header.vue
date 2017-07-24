caac9771fb217f8257c4ff5103346bef504b453e
linzi <linzi4235@gmail.com>
2017-07-24 17:57:08 +0800

    edit header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -44,13 +44,22 @@
             <div class="line"></div>
           </div>
           <ul v-if="seller.supports" class="supports">
-            <li class="support-item" v-for="item in seller.supports"></li>
+            <li class="support-item" v-for="item in seller.supports" track-by="$index" >
             <span class="icon" :class="classMap[seller.supports[$index].type]"></span>
-            <span class="text">{{seller.suports[$index].description}}</span>
+            <span class="text">{{seller.supports[$index].description}}</span>
+            </li>
           </ul>
+          <div class="title">
+            <div class="line"></div>
+                <div class="text">商家信息</div>
+            <div class="line"></div>
+          </div>
+          <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+          </div>
         </div>
       </div>
-      <div class="detail-close">
+      <div class="detail-close" @click="hideDetail">
         <i class="icon-close"></i>
       </div>
     </div>
@@ -206,8 +215,42 @@
               font-weight 700
               padding:0 12px
               font-size: 12px
-
-
+          .supports
+              width:80%
+              margin: 0 auto
+              .support-item
+                padding:0 12px
+                margin-bottom:12px
+                font-size:0
+                &:last-child
+                    margin-bottom:0
+                .icon
+                    display:inline-block
+                    width:16px
+                    height:16px
+                    vertical-align:top
+                    margin-right:6px
+                    background-size:16px 16px
+                    background-repeat:no-repeat
+                    &.decrease
+                      bg-image('decrease_2')
+                    &.discount
+                      bg-image('discount_2')
+                    &.guarantee
+                      bg-image('guarantee_2')
+                    &.invoice
+                      bg-image('invoice_2')
+                    &.special
+                        bg-image('special_2')
+                .text
+                    line-height:16px
+                    font-size:12px
+          .bulletin
+            width:80%
+            margin:0 auto
+            .content
+                font-size:12px
+                line-height:24px
       .detail-close
         position: relative
         width: 32px
@@ -228,6 +271,9 @@
     methods: {
       showDetail () {
         this.detailShow = true;
+      },
+      hideDetail () {
+        this.detailShow = false;
       }
     },
     props: {
