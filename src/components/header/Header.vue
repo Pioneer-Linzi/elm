731ad4b52910f4370acc1370cc98e0d91ee58cd7
linzi <linzi4235@gmail.com>
2017-07-24 19:01:14 +0800

    add

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -33,7 +33,7 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height='100%' ; alt="">
     </div>
-    <div v-show="detailShow" class="detail">
+    <div v-show="detailShow" class="detail" transition="fade">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
@@ -186,7 +186,14 @@
       width: 100%
       height: 100%
       overflow: auto
-      background: rgba(7, 17, 27, 0.8)
+      transition: all 0.5s
+      backdrop-filter:blur(10px)
+      &.fade-transition
+        opacity: 1
+        background: rgba(7, 17, 27, 0.8)
+      &.fade-enter,&.fade-leave
+        opacity:0
+        background: rgba(7, 17, 27, 0)
       .detail-wrapper
         min-height: 100%
         width: 100%
