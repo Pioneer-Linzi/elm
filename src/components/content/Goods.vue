d012532b3d735f016c075bab65fd375d7c3d41c6
linzi <linzi4235@gmail.com>
2017-08-03 17:47:43 +0800

    add goods

--- src/components/content/Goods.vue
+++ src/components/content/Goods.vue
@@ -1,11 +1,26 @@
 <template>
-<div class="goods">
-    this is goods panel
-</div>
+	<div class="goods">
+		<div class="menu-wrapper"></div>
+		<div class="food-wrapper"></div>
+	</div>
 </template>
 <style scoped lang="stylus" rel="stylesheet/stylus">
+.goods
+	display:flex
+	position:absolute
+	top:174px
+	width:100%
+	bottom:46px
+	overflow:hidden
+	.menu-wrapper
+		flex:0 0 80px;
+		width:80px
+		background:#f3f5f7
+	.food-wrapper
+		flex:1
+
 </style>
 <script type="text-ecmascript-6">
-    export default {};
+export default {};
 </script>
 
