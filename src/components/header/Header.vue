e4fa41667a9cffe4e009402d9db5df1aaae7e11a
linzi <linlin_zhao@yeah.net>
2017-08-03 22:21:31 +0800

    fix stylus

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -4,8 +4,7 @@
 			<div class="avatar">
 				<img :src="seller.avatar" width="64" height="64" alt=""/>
 			</div>
-			<div class="content">
-				<div class="title">
+			<div class="content"> <div class="title">
 					<span class="brand">
 					</span>
 					<span class="name">
@@ -44,8 +43,7 @@
 						<div class="line"></div>
 					</div>
 					<ul v-if="seller.supports" class="supports">
-						<li class="support-item" v-for="item in
-							seller.supports" track-by="$index">
+						<li class="support-item" v-for="item in seller.supports" track-by="$index">
 							<span class="icon" :class="classMap[seller.supports[$index].type]"></span>
 							<span class="text">{{seller.supports[$index].description}}</span>
 						</li>
@@ -116,104 +114,104 @@
 								bg-image('invoice_1')
 						.text
 							line-height: 12px
-					.support-count
-							position: absolute
-							right: 12px
-							bottom: 18px
-							padding: 0 8px
-							height: 24px
-																border-radius: 14px
-																background-color: rgba(0, 0, 0, 0.2)
-																text-align: center
-																		.count
-																			vertical-align: middle
-																			font-size: 10px
-																			line-height: 28px
-																			.icon-keyboard_arrow_right
-																				line-height: 24px
-																				margin-left: 2px
-																				vertical-align: middle
-																				font-size: 10px
-																				.bulletin-wrapper
-																					height: 28px
-																					line-height: 28px
-																					padding: 0 22px 0 12px
-																					white-space: nowrap
-																					overflow: hidden
-																					-ms-text-overflow: ellipsis
-																					text-overflow: ellipsis
-																					position: relative;
-																					background: rgba(7, 17, 27, 0.2)
-																					.bulletin-title
-																						display: inline-block
-																						width: 22px
-																						height: 12px;
-																						vertical-align: top
-																						bg-image('bulletin')
-																						background-size: 22px 12px
-																						margin-top: 8px
-																						background-repeat no-repeat
-																						.bulletin-text
-																							vertical-align: top
-																							margin: 0 4px
-																							font-size: 10px
-																							.icon-keyboard_arrow_right
-																								position: absolute
-																								font-size: 10px
-																								right: 12px
-																								top: 8px
-																								.background
-																									position: absolute
-																									top: 0
-																									left: 0
-																									width: 100%
-																									height: 100%
-																									z-index: -1
-																									filter: blur(10px)
-																									.detail
-																										position: fixed
-																										z-index: 100
-																										top: 0
-																										left: 0
-																										width: 100%
-																										height: 100%
-																										overflow: auto
-																										background: rgba(7, 17, 27, 0.8)
-																										.detail-wrapper
-																											min-height: 100%
-																											width: 100%
-																											.detail-main
-																												margin-top: 64px
-																												padding-bottom: 64px
-																												.name
-																													line-height: 16px
-																													text-align: center
-																													font-size: 16px
-																													font-weight: 700
-																													.star-wrapper
-																														margin-top:18px
-																														padding:2px 0
-																														text-align :center
-																														.title
-																															display: flex
-																															width:80%
-																															margin:28px auto 24px auto
-																															.line
-																																flex:1
-																																position relative
-																																top:-6px
-																																border-bottom:1px solid rgba(255,255,255,0.2)
-																																.text
-																																	font-weight 700
-																																	padding:0 12px
-																																	font-size: 12px
-																																	.detail-close
-																																		position: relative
-																																		width: 32px
-																																		height: 32px
-																																		margin: -64px auto
-																																		clear: both
-																																		font-size: 32px
+			.support-count
+				position: absolute
+				right: 12px
+				bottom: 18px
+				padding: 0 8px
+				height: 24px
+				border-radius: 14px
+				background-color: rgba(0, 0, 0, 0.2)
+				text-align: center
+				.count
+					vertical-align: middle
+					font-size: 10px
+					line-height: 28px
+				.icon-keyboard_arrow_right
+					line-height: 24px
+					margin-left: 2px
+					vertical-align: middle
+					font-size: 10px
+		.bulletin-wrapper
+			height: 28px
+			line-height: 28px
+			padding: 0 22px 0 12px
+			white-space: nowrap
+			overflow: hidden
+			-ms-text-overflow: ellipsis
+			text-overflow: ellipsis
+			position: relative;
+			background: rgba(7, 17, 27, 0.2)
+			.bulletin-title
+				display: inline-block
+				width: 22px
+				height: 12px;
+				vertical-align: top
+				bg-image('bulletin')
+				background-size: 22px 12px
+				margin-top: 8px
+				background-repeat no-repeat
+			.bulletin-text
+				vertical-align: top
+				margin: 0 4px
+				font-size: 10px
+			.icon-keyboard_arrow_right
+				position: absolute
+				font-size: 10px
+				right: 12px
+				top: 8px
+		.background
+			position: absolute
+			top: 0
+			left: 0
+			width: 100%
+			height: 100%
+			z-index: -1
+			filter: blur(10px)
+		.detail
+			position: fixed
+			z-index: 100
+			top: 0
+			left: 0
+			width: 100%
+			height: 100%
+			overflow: auto
+			background: rgba(7, 17, 27, 0.8)
+			.detail-wrapper
+				min-height: 100%
+				width: 100%
+				.detail-main
+					margin-top: 64px
+					padding-bottom: 64px
+					.name
+						line-height: 16px
+						text-align: center
+						font-size: 16px
+						font-weight: 700
+					.star-wrapper
+						margin-top:18px
+						padding:2px 0
+						text-align :center
+					.title
+						display: flex
+						width:80%
+						margin:28px auto 24px auto
+						.line
+							flex:1
+							position relative
+							top:-6px
+							border-bottom:1px solid rgba(255,255,255,0.2)
+						.text
+							font-weight 700
+							padding:0 12px
+							font-size: 12px
+		.detail-close
+			position: relative
+			width: 32px
+			height: 32px
+			margin: -64px auto
+			clear: both
+			font-size: 32px
 </style>
 <script>
 import star from 'components/star/star';
